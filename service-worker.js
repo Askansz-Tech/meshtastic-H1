16f2a423a6e2ebb20dc8aa8979f1274e0a7a4363
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2025-08-02 23:33:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,38 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("qr-code-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+const CACHE_NAME = "qr-code-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "../icons/qr-icon-512.png",
+  "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
   );
 });
